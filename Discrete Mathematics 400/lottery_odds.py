db9880224ac929aaf12995664891bacb157d04f3
Khaled Adad <khaledadad@lewisu.edu>
2022-04-06 23:22:12 -0500

    wip

--- Discrete Mathematics 400/lottery_odds.py
+++ Discrete Mathematics 400/lottery_odds.py
@@ -1,3 +1,14 @@
 def factorial(n):
     if n == 0:
         return 1
+    return n * factorial(n-1)
+    
+def choose(n, k):
+    return factorial(n)//(factorial(k)*factorial(n-k))
+
+# illinois lottery
+print(choose(52,6))
+
+print(choose(100,20))
+
+print(choose(100,50))
