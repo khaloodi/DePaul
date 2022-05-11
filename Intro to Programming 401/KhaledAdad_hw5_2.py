cc4cc80d3ccf44462e6dbd7e8aebaa84c262a336
Khaled Adad <khaledadad@lewisu.edu>
2022-05-10 22:29:51 -0500

    problem 2 completed

--- Intro to Programming 401/KhaledAdad_hw5_2.py
+++ Intro to Programming 401/KhaledAdad_hw5_2.py
@@ -1,3 +1,26 @@
 # Khaled Adad
 # HW 5 Problem 2
 
+def upper_1():
+
+    words = []
+    upperCaseFirstLetter = 0
+
+    while True:
+        prompt = (input('Enter some a word: '))
+
+        if prompt == '' or ' ' in prompt:
+            break
+
+        if prompt[0] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            upperCaseFirstLetter += 1
+        words.append(prompt)
+        print(words)
+
+    if len(words) == 0:
+        return '0 words were entered'
+
+    return f'Words with upper case first letter = {upperCaseFirstLetter}'
+
+print(upper_1())
+    
