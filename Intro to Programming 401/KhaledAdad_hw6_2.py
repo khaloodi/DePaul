1177fabbfd58e9e95a4ddf0b52d7681d00444944
Khaled Adad <khaledadad@lewisu.edu>
2022-05-17 23:22:14 -0500

    stuck on problem 2

--- Intro to Programming 401/KhaledAdad_hw6_2.py
+++ Intro to Programming 401/KhaledAdad_hw6_2.py
@@ -1,3 +1,40 @@
 # Khaled Adad
 # HW 6 Problem 2
+import string
 
+def wordSearch(fname, wordLst):
+    f = open(fname, 'r')
+    print(f)
+    lines = len(f.readlines())
+    print(lines)
+    accumulator = 0
+
+    index = {}
+    for word in wordLst:
+        index[word] = []
+    print(f'Initial index: {index}')
+    p = string.punctuation.replace("'", " ")
+    print(f'P is: {p}')
+
+    line = f.readline()
+    print(f'My first line: {line}')
+    while line:
+        print(f'My second line: {line}')
+        accumulator += 1
+        words = line.split()
+        for char in p:
+            if char in words:
+                print(true)
+                words.remove(char)
+
+        new_line = ' '.join(words)
+
+        for word in wordLst:
+            if word in new_line:
+                index[word].append(accumulator)       
+        print(new_line)          
+        line = f.readline()
+    f.close()
+    return index
+
+print(wordSearch('Pride_and_Prejudice.txt', ["Kent"]))
